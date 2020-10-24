970c7b4445763e2e965ed0d15ce42e4257d5d67b
ashwini sakthivel <ashsakthi.code@gmail.com>
2020-10-24 08:44:12 -0400

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 		<h1>Loading</h1> : 
 		(
 			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
+				<h1 className='f1'>RoboSquad</h1>
 				<SearchBox searchChange={this.onSearchChange} />
 				<Scroll>
 				<CardList robots={filteredRobots} />
